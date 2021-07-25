61afea309d773a09b3580de54f8badf669681915
Mitchell Gohman <meatch@me.com>
2021-07-25 07:41:33 -0700

    ReOrg: Moving common into React

--- src/React/Test.jsx
+++ src/React/Test.jsx
@@ -1,28 +1,26 @@
 /*===================================
-|| 
+||
 || This is just to test some of the features of this app
 || More than likely you will be removing this for your app
-|| 
+||
 ===================================*/
-
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 /* Scipts ---------------------------*/
-import API from 'common/API.js';
+import api from 'React/common/api.js';
 
 const Test = () => {
 
     useEffect(() => {
-        API.get('/staff/').then((response) => {
+        api.get('/staff/').then((response) => {
             console.log('response', response);
         });
     }, []);
 
-
     return (
         <TestStyled className='Test'>
-            Test 
+            Test
         </TestStyled>
     );
 }
--- src/React/common/api.js
+++ src/React/common/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const baseURL = (process.env.REACT_APP_URL) ? `${process.env.REACT_APP_URL}/api` : `/api`;
 
-const API = axios.create({
+const api = axios.create({
     baseURL: baseURL
 });
 
-export default API;
+export default api;
