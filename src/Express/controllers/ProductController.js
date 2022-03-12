cbed1ffd0f57bb933905ad6932c02f256fecf935
Mitch Gohman <meatched@gmail.com>
2022-03-11 18:03:05 -0700

    Build Project: Products All and byID

--- src/Express/controllers/ProductController.js
+++ src/Express/controllers/ProductController.js
@@ -1,47 +1,29 @@
 require('../common/mongoose.conn.js');
 const Product = require('../models/Product.js');
-const ProductType = require('../models/ProductType.js');
 const reqResp = require('../common/reqResp.js');
 
 const ProductController = () => {
 
     /* User: All ---------------------------*/
     const all = reqResp(({handleResponse, handleError}) => {
-        Product.find({},(err, dbProducts) => {
-            if (err) handleError(err);
-
-            // const groomedProducts = dbProducts.map(async(product) => {
-            //     const groomedProduct = await addProductType(product);
-            //     return groomedProduct;
-            // });
-
-            handleResponse(dbProducts, 'All products retrieved.');
-        });
+        Product.find({})
+            .exec((err, dbProducts) => {
+                if (err) handleError(err);
+                handleResponse(dbProducts, 'All products retrieved.');
+            });
     });
 
     /* User: byID ---------------------------*/
     const byID = reqResp(({reqParams, handleResponse, handleError}) => {
         if (reqParams.id) {
-            // Make sure account exists
-            Product.findOne({ "_id": reqParams.id },(err, dbProducts) => {
+            Product.findOne({ "_id": reqParams.id },(err, dbProduct) => {
                 if (err) handleError(err);
-                handleResponse(dbProducts, 'Product retrieved.');
+                handleResponse(dbProduct, 'Product retrieved.');
             });
         } else {
             handleResponse({_id: expReq.params.id, body: reqUser}, 'Unable to locate product by productID.', false);
         }
     });
-
-    const addProductType = async (product) => {
-
-        let groomedProduct = product;
-
-        await ProductType.find({ prodTypeID: product.prodTypeID },(err, productType) => {
-            groomedProduct.productType = productType;
-        });
-
-        return groomedProduct;
-    }
 
     return {
         all: all,
--- src/Express/models/Product.js
+++ src/Express/models/Product.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 
 //Define a schema for our Model
 const ProductSchema = new mongoose.Schema({
-    prodTypeID: { type: Number, required: 'Product prodTypeID is required', },
     title: { type: String, required: 'Product title is required', },
     description: { type: String, },
     image: { type: String, required: 'Product image is required', },
     price: { type: Number, required: 'Product price is required', },
+    prodType: { type: String, required: 'Product prodType is required', },
 });
 
 // Remove sensitive data in responses.
