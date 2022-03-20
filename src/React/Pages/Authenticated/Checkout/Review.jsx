7c865232ea7c2a2e9465900370e276c780d3a1f5
Mitch Gohman <meatched@gmail.com>
2022-03-19 20:03:32 -0600

    Build Project: Pushing back to payment page when step is locked.

--- src/React/Pages/Authenticated/Checkout/Review.jsx
+++ src/React/Pages/Authenticated/Checkout/Review.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const Review = () => {
+
+    const { checkout } = useSelector(state => state);
+    const history = useHistory();
+
+    const step = checkout.find((step) => step.step === 2);
+
+    useEffect(() => {
+        if (!step.unlocked) {
+            history.push('/auth/checkout/payment');
+        }
+    }, [step]);
+
     return (
         <ReviewStyled className='Review'>
             <h1>Review</h1>
--- src/React/Pages/Authenticated/Checkout/Thanks.jsx
+++ src/React/Pages/Authenticated/Checkout/Thanks.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect} from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 const Thanks = () => {
+
+    const { checkout } = useSelector(state => state);
+    const history = useHistory();
+
+    const step = checkout.find((step) => step.step === 3);
+
+    useEffect(() => {
+        if (!step.unlocked) {
+            history.push('/auth/checkout/review');
+        }
+    }, [step]);
+
     return (
         <ThanksStyled className='Thanks'>
             <h1>Thanks</h1>
