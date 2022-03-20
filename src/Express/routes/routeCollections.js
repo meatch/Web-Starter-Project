fbee2fda3521d26c95e037e574139445687a7613
Mitch Gohman <meatched@gmail.com>
2022-03-20 14:44:19 -0600

    Express: Initialize routes for checkout payment and review

--- src/Express/routes/routeCollections.js
+++ src/Express/routes/routeCollections.js
@@ -12,43 +12,43 @@ const routeCollections = {
             resource: '/preloadState',
             controller: Controllers.PreloadStateController,
             routes: [
-                { method: "get",     handler: "preloadState",            path: `/`,                  },
+                { method: "get",     handler: "preloadState",            path: `/`,                     },
             ],
         },
         {
             resource: '/sample',
             controller: Controllers.SampleController,
             routes: [
-                { method: "get",     handler: "getSample",              path: `/`,                  },
+                { method: "get",     handler: "getSample",              path: `/`,                      },
             ],
         },
         {
             resource: '/login',
             controller: Controllers.LoginController,
             routes: [
-                { method: "post",     handler: "login",                 path: `/`,                  },
+                { method: "post",     handler: "login",                 path: `/`,                      },
             ],
         },
         {
             resource: '/logout',
             controller: Controllers.LoginController,
             routes: [
-                { method: "get",      handler: "logout",                path: `/`,                  },
+                { method: "get",      handler: "logout",                path: `/`,                      },
             ],
         },
         {
             resource: '/users',
             controller: Controllers.UserController,
             routes: [
-                { method: "post",    handler: "create",                 path: `/`,                 },
+                { method: "post",    handler: "create",                 path: `/`,                      },
             ],
         },
         {
             resource: '/products',
             controller: Controllers.ProductController,
             routes: [
-                { method: "get",     handler: "all",                    path: `/`,                 },
-                { method: "get",     handler: "byID",                   path: `/:id`,              },
+                { method: "get",     handler: "all",                    path: `/`,                      },
+                { method: "get",     handler: "byID",                   path: `/:id`,                   },
             ],
         },
     ],
@@ -57,19 +57,27 @@ const routeCollections = {
             resource: '/users',
             controller: Controllers.UserController,
             routes: [
-                { method: "get",     handler: "all",                    path: `/`,                 },
-                { method: "get",     handler: "byID",                   path: `/:id`,              },
-                { method: "patch",   handler: "update",                 path: `/:id`,              },
-                { method: "patch",   handler: "updatePassword",         path: `/:id/password`,     },
+                { method: "get",     handler: "all",                    path: `/`,                      },
+                { method: "get",     handler: "byID",                   path: `/:id`,                   },
+                { method: "patch",   handler: "update",                 path: `/:id`,                   },
+                { method: "patch",   handler: "updatePassword",         path: `/:id/password`,          },
+            ],
+        },
+        {
+            resource: '/checkout',
+            controller: Controllers.CheckoutController,
+            routes: [
+                { method: "post",     handler: "payment",               path: `/payment`,               },
+                { method: "post",     handler: "review",                path: `/review`,                },
             ],
         },
         {
             resource: '/orders',
             controller: Controllers.OrderController,
             routes: [
-                { method: "post",     handler: "create",                path: `/`,                 },
-                { method: "get",      handler: "getCart",               path: `/:userID`,          },
-                { method: "patch",    handler: "purchase",              path: `/purchase/:userID`, },
+                { method: "post",     handler: "create",                path: `/`,                      },
+                { method: "get",      handler: "getCart",               path: `/:userID`,               },
+                { method: "patch",    handler: "purchase",              path: `/purchase/:userID`,      },
             ],
         },
     ],
