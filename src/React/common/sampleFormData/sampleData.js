52fb6dd12345f9d1a0da7df181f0dc3874effe0a
Mitch Gohman <meatched@gmail.com>
2022-03-21 22:53:10 -0600

    Sample Form Data Manager Script

---
+++ src/React/common/sampleFormData/sampleData.js
@@ -0,0 +1,7 @@
+export default [
+    { key: "first",                     value: 'Mitch'                  },
+    { key: "last",                      value: 'Gohman'                 },
+    { key: "email",                     value: 'mitch@me.com'           },
+    { key: "password",                  value: '123456'                 },
+    { key: "password-confirmation",     value: '654321'                 },
+]
