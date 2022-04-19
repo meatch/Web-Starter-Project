68cc2662a67531562c589672c3d98d48e00486e6
Mitch Gohman <meatched@gmail.com>
2022-04-19 16:40:11 -0600

    Removed Redux Dev Tools from Production

--- src/Redux/store/composeEnhancers.js
+++ src/Redux/store/composeEnhancers.js
@@ -3,6 +3,7 @@ import ReduxThunk from 'redux-thunk';
 
 // Redux Dev Tools
 const enhancers =
+    process.env.NODE_ENV !== "production" &&
     typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({name: 'CrossFit Decimate'}) :
