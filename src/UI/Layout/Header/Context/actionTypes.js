e32b5239d3a331e6ccb3624871be81d07c390ec2
Mitch Gohman <meatched@gmail.com>
2022-06-18 16:56:10 -0600

    Web 1: Resetting to Web 1 Starter Files Only

--- src/UI/Layout/Header/Context/actionTypes.js
+++
@@ -1,7 +0,0 @@
-/*---------------------------
-| Action Types (TOC)
----------------------------*/
-export const actionTypes = {
-    NAV_UPDATE_IS_SMALL: 'NAV: updateIsSmall',
-    NAV_UPDATE_SHOW_MOBILE_MENU: 'NAV: updateShowMobileMenu',
-}
--- src/UI/UI.jsx
+++ src/UI/UI.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
-
-/* Components ---------------------------*/
-import Layout from './Layout/Layout.jsx';
-import Routing from './Routing.jsx';
+import styled from 'styled-components';
 
 const UI = () => {
     return (
-        <Layout>
-            <Routing />
-        </Layout>
+        <UIStyled className='UI'>
+            <h1>Web Starter Project</h1>
+            <div className="student-name">
+                STUDENT NAME
+            </div>
+        </UIStyled>
     );
 }
 
 export default UI;
+
+const UIStyled = styled.div`
+    background-color: teal;
+    margin: 50px auto;
+    padding: 50px;
+    max-width: 500px;
+    text-align: center;
+
+    .student-name {
+        color: white;
+        font-size: 30px;
+    }
+`;
