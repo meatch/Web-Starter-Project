bdd5b9aaae3bc21b1c060e103dc42c0f2c741b0e
Mitch Gohman <meatched@gmail.com>
2022-06-18 16:31:31 -0600

    Web 1: Removing Excess in Express server

--- express.js
+++ express.js
@@ -13,29 +13,6 @@ require('dotenv').config();
 ---------------------------*/
 const path = require('path');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const cors = require('cors');
-
-/*---------------------------
-| Mongoose
----------------------------*/
-const mongoose = require('mongoose');
-mongoose.promise = global.Promise;
-const mongoConn = process.env.MONGO_DB_CONN;
-/* Connecting to Mongo ---------------------------*/
-if (mongoConn) {
-    mongoose
-        .connect(mongoConn)
-        .then((res) => {
-            console.log('Mongoose: Connection made.');
-        })
-        .catch((err) => {
-            console.log(`Mongoose Connection Error: ${err}`);
-        });
-} else {
-    console.log('Missing MONGO_DB_CONN .env var for Mongoose connection');
-}
 
 /*---------------------------
 | Express
@@ -46,46 +23,8 @@ const app = express();
 /* JSON support for request response ---------------------------*/
 app.use(express.json());
 
-/* Server Session support user access. ---------------------------*/
-// initialize cookie-parser to allow us access the cookies stored in the browser.
-app.use(cookieParser());
-const oneDay = 1000 * 60 * 60 * 24;
-app.use(session({
-    secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        expires: oneDay,
-        maxAge: oneDay,
-    },
-}));
-
 /* Serve the static files from the React app ---------------------------*/
 app.use(express.static(path.join(__dirname, 'build')));
-
-/*---------------------------
-| !IMPORTANT :: Should not be done in Production
-| Bypassing CORS so Node Express can be on NODE_PORT 5000 and react can be on 3000
----------------------------*/
-if (process.env.NODE_ENV === 'development') {
-    console.log('Bypassing CORS for Local Development.');
-
-    const appUrl = process.env.REACT_APP;
-
-    const corsOptions = {
-        origin: appUrl,  //Your Client, do not write '*'
-        credentials: true,
-    };
-
-    console.log('corsOptions', corsOptions);
-    app.use(cors(corsOptions));
-}
-
-/*---------------------------
-| Route Collections
----------------------------*/
-const renderRoutes = require('./src/Express/routes/renderRoutes.js');
-renderRoutes(app);
 
 /*---------------------------
 | Port Management
